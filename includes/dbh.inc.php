1f719c6ad0ea69ce9bdd67170fc9aeddaf24eb29
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-05-22 21:17:26 +0800

    Connect to a Database

---
+++ includes/dbh.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=myfirstdatabase";
+$dbusername = "root";
+$dbpassword = "";
+
+
+try {
+  $pdo = new PDO($dsn, $dbusername, $dbpassword);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo "Connection Failed" . $e->getMessage();
+}
+
+
