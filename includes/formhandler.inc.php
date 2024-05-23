9a44cdd6184e3a90f174ae2cbaea60e4546f242a
shugaKaneDy <ahnyudaengz5@gmail.com>
2024-05-23 22:08:37 +0800

    Insert into database using php

---
+++ includes/formhandler.inc.php
@@ -0,0 +1,41 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+  $username = $_POST['username'];
+  $pwd = $_POST['pwd'];
+  $email = $_POST['email'];
+
+  try{
+    require_once "dbh.inc.php";
+
+
+    // non-name parameters
+    // $query = "INSERT INTO users (username, pwd, email) VALUES (?, ?, ?);";
+    // $stmt = $pdo->prepare($query);
+    // $stmt->execute([$username, $pwd, $email]);
+
+    // $pdo = null;
+    // $stmt = null;
+
+    // name parameters
+    $query = "INSERT INTO users (username, pwd, email) VALUES (:username, :pwd, :email);";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(":username", $username);
+    $stmt->bindParam(":pwd", $pwd);
+    $stmt->bindParam(":email", $email);
+
+    $stmt->execute();
+
+    $pdo = null;
+    $stmt = null;
+
+    header("Location: ../index.php");
+
+    die();
+  } catch (PDOException $e) {
+    die("Query failed: " . $e->getMessage());
+  }
+
+} else {
+  header("Location: ../index.php");
+}
